15f321f75a7c2c5e90f95fdd6b73476632bad7d7
williamchidi <williamchidi1@gmail.com>
2024-06-08 12:03:06 +0100

    u

--- ItemInCartContainer.jsx
+++ ItemInCartContainer.jsx
@@ -4,7 +4,7 @@ import ShoppingCartItemCard from './src/features/card/ShoppingCartItemCard';
 function ItemInCartContainer({ title }) {
   return (
     <div className="item-container">
-      <h2 className="display-Title">{title}</h2>
+      <h2 className="display-Title-cart-item">{title}</h2>
       <div className="item-in-cart-container">
         <ShoppingCartItemCard />
       </div>
